38d4aac1c7c63df9048d18990b2ae0f9ea250b85
Rafael Ito <rafaelito91@gmail.com>
2025-01-31 12:57:32 -0300

    remove dist

--- dist/utils/greeting.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.saudacao = void 0;
-const saudacao = () => {
-    console.log('teste');
-};
-exports.saudacao = saudacao;
-//# sourceMappingURL=greeting.js.map
