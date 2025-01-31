6a770797be63fa738f52922ae8eec21e7a2afd49
Leon Suckow <leonlucassuckow@hotmail.com>
2025-01-31 12:34:21 -0300

    Configured typescript in the project

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import express from "express";
 // const greeting = require("./utils/greeting.js");
+
 // greeting.saudacao();
+
 const app = express();
 const port = 3333;
+
 app.get("/", (request, response) => {
-    response.send("LipinhopGod");
+  response.send("");
 });
+
 app.listen(port, () => {
-    console.log(`Application running localhost:${port}`);
+  console.log(`Application running localhost:${port}`);
 });
