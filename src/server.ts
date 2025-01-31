dfee5dd22946aceee0649eb1c863fc3356c213e1
Leon Suckow <leonlucassuckow@hotmail.com>
2025-01-31 13:13:40 -0300

    user router and @types/express

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,33 @@
-import {saudacao} from "./utils/greeting";
-import express from 'express';
+import express from "express";
 
-saudacao()
 const app = express();
 const port = 3333;
 
+const users = [
+  { name: "LipinhoFire", idade: 39 },
+  { name: "ColomboFire", idade: 57 },
+  { name: "LeonFire", idade: 29 },
+  { name: "FalshionFire", idade: 34 },
+];
+
+const userRouter = express.Router();
+
+userRouter.get("/user", (request, response) => {
+  response.send(users);
+});
+
+userRouter.post("/user", (request, response) => {
+  const { name, idade } = request.body;
+
+  const newUser = { name, idade };
+  users.push(newUser);
+  response.sendStatus(200).send(newUser);
+});
+
+userRouter.delete("/user/:id", (reuqest, response) => {});
+
+app.use(userRouter);
+
 app.listen(port, () => {
-  console.log(`Application running localhost:${port}`);
+  console.log(`Application running http://localhost:${port} 🚀🚀🚀`);
 });
